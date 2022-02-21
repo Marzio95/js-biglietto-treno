ed9e2859b132c080309258f50d8eeb1aa438f146
Marzio95 <marziodellarocca14@gmail.com>
2022-02-21 17:26:49 +0100

    creo un layout funzionante con js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,20 @@
 let km = prompt('Kilometri da percorrere:');
+document.getElementById('KilometriInseriti').innerHTML = km;
+
 let age = prompt('Anni passeggero:');
+document.getElementById('anniInseriti').innerHTML = age;
 
 const prezzoKm = 0.21;
+document.getElementById('prezzoAlKilometro').innerHTML = prezzoKm;
+
+const scontoGiovani = 20;
+document.getElementById('sconto_inferiore18anni').innerHTML = scontoGiovani;
+
+const scontoAnziani = 40;
+document.getElementById('sconto_superiore65anni').innerHTML = scontoAnziani
 
 let molt = km * prezzoKm;
-document.getElementById('prezzo_nosconto').innerHTML = molt
+document.getElementById('prezzo_nosconto').innerHTML = molt;
 
 
 if (age < 18) {
